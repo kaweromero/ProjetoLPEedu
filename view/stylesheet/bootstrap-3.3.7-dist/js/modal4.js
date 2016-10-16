c6858abf1a779d342786493f35f7b6eff26aa9b3
Jaquelinne <amorim.jaquelinne@gmail.com>
2016-10-16 14:37:52 -0300

    Modal para a tela do guia do jogo

---
+++ view/stylesheet/bootstrap-3.3.7-dist/js/modal4.js
@@ -0,0 +1,21 @@
+// Get the modal
+var modal4 = document.getElementById('myModal4');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img4 = document.getElementById('myImg4');
+var modalImg4 = document.getElementById("img04");
+var captionText4 = document.getElementById("caption4");
+img4.onclick = function(){
+    modal4.style.display = "block";
+    modalImg4.src = this.src;
+    captionText4.innerHTML = this.alt;
+}
+
+
+// Get the <span> element that closes the modal
+var span4 = document.getElementsByClassName("close4")[0];
+
+// When the user clicks on <span> (x), close the modal
+span4.onclick = function() {
+  modal4.style.display = "none";
+}
